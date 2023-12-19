a7e2b978f759fd32930af4878660223f9b6eeb1f
Shubham Jangle <jangleshubham13@gmail.com>
2023-12-19 21:37:49 +0530

    added carousel component

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  A11y,
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
+import "swiper/css/bundle";
+
+const carouselApiResponse = [
+  "/image1.png",
+  "/image2.png",
+  "/image3.png",
+  "/image4.png",
+];
 
 function Carousel() {
-  return <div>Carousel</div>;
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      modules={[Navigation, Pagination, Scrollbar, Autoplay, A11y, EffectFade]}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{ delay: 2000 }}
+      effect="fade"
+    >
+      {carouselApiResponse.map((imageSrc, index) => (
+        <SwiperSlide key={index}>
+          <img
+            className="h-[80vh] w-[100%] object-cover p-2"
+            src={imageSrc}
+            alt="carousel"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
 export default Carousel;
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
   return (
     <div className="h-14 bg-blue-500">
-      <div className="container max-w-6xl mx-auto text-2xl font-bold py-3 text-white">
+      <div className="container max-w-6xl mx-auto text-2xl font-bold p-3 text-white">
         React Carousel
       </div>
     </div>
