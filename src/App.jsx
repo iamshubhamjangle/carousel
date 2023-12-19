925c124a3ebf7932e2e0927b29457c03144d4c00
Shubham Jangle <jangleshubham13@gmail.com>
2023-12-19 21:07:11 +0530

    added images

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Carousel from "./components/carousel";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Carousel />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="h-14 bg-blue-500">
+      <div className="container max-w-6xl mx-auto text-2xl font-bold py-3 text-white">
+        React Carousel
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
